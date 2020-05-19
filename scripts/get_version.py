852caf2661f365321e0e572d02c37bae758b6f37
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-05-20 01:45:42 +0200

    test python file 9

--- scripts/get_version.py
+++ scripts/get_version.py
@@ -2,7 +2,18 @@
 import sys
 
 
-if sys.argv[0]=='hi':
-    print('Salaam')
-sys.exit(0)
+def main():
+    if sys.argv[0] == 'hi':
+        print('Salaam')
+        print ("exec main..")
+        sys.stderr.write('execution ok\n')
+        sys.exit(0)
+    return "execution ok"
+
+if __name__ == '__main__':
+    main()
+
+
+
+
 
